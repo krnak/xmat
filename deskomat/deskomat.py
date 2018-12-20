817f0b21c3420d47c61a31c64cb0bbf2f89e273f
jarys <epfs@seznam.cz>
2018-12-20 22:34:09 +0100

    deskomat email...

--- deskomat/deskomat.py
+++ deskomat/deskomat.py
@@ -7,7 +7,6 @@ from xmat import Xmat
 
 source_xpath = "//table[@class='content edeska-vypis-vyveseni']/tbody/tr/td[@class='content edeska-sloupec-nazev']/a"
 url = 'http://cztedeskaext.praha4.cz/eDeska/eDeskaAktualni.jsp'
-
 class Deskomat(Xmat):
 	def __init__(self):
 		super().__init__('deskomat')
@@ -23,11 +22,16 @@ class Deskomat(Xmat):
 		self.mqtt_client.connect("mqtt.KRNAK", 1883, 1000)
 		self.mqtt_client.loop_start()
 
+		self.target_email = self.setting["email"]
+		if not self.target_email:
+			self.target_email = input('Zadej cílovou emailovou adresu: ')
+
 	def items_of_interest(self):
 		try:
 			deska = requests.get(url, timeout=10)
-		except requests.exceptions.Timeout:
-			return
+		except Exception as e:
+			self.send("tomas@krnak.cz")
+			return 
 			
 		tree = html.fromstring(deska.content)
 		for anchor in tree.xpath(source_xpath):
@@ -44,7 +48,7 @@ class Deskomat(Xmat):
 				if word in text.lower():
 					subject = '[deskomat]: {} nalezeno'.format(word)
 					text += '\n' + link
-					yield (self.setting["email"], subject, text)
+					yield (self.target_email, subject, text)
 
 if __name__ == '__main__':
 	Deskomat().loop()
