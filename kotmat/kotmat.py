b050f9404bb91568613fc3d590b39b85fcc921f4
Tomas Krnak <tomas@krnak.cz>
2020-01-31 15:44:26 +0100

    try + email + price check

--- kotmat/kotmat.py
+++ kotmat/kotmat.py
@@ -7,7 +7,8 @@ sys.path.append("..")
 from xmat import Xmat
 import db
 
-address = db.load_or_write("sensitive", "sms_email")
+address = db.load_or_write("sensitive", "email")
+# address = db.load_or_write("sensitive", "sms_email")
 url = 'https://icts.kuleuven.be/apps/kotwijs/search/index.php'
 headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 with open("raw_data.txt") as file:
@@ -31,10 +32,17 @@ class Kotmat(Xmat):
 				data=raw_data.format(i),
 				headers=headers
 			)
-			xml_tree = xml.fromstring(r.content)
-			html_etree = html.fromstring(xml_tree.getchildren()[0].getchildren()[-4].text)
+
+			try:
+				xml_tree = xml.fromstring(r.content)
+				html_etree = html.fromstring(xml_tree.getchildren()[0].getchildren()[-4].text)
+			except Error as e:
+				self.send_error(e, address)
+
 			for kot in html_etree.xpath('//div[@class="dtr_kamer kamer result"]'):
 				price = kot.xpath('.//div[@class="prijs result__price"]')[0].text_content()
+				if float(price) > 400:
+					continue
 				iid   = kot.xpath('./a')[0].attrib["onclick"]
 				if not iid in self.db["ids"]:
 					self.db["ids"].append(iid)
